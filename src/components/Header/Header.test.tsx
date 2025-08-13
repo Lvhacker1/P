f5909de25692d4eb68deea00f9616ceb131715f8
Lvhacker1 <lavasha2@hotmail.com>
2025-08-13 16:20:24 +0200

    remove test

--- src/components/Header/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen} from '@testing-library/react';
-import Header from './index';
-
-test('renders header text', () => {
-  render(<Header />);
-  
-  expect(screen.getByRole('heading', { name: /blablabla/i })).toBeInTheDocument();
-});
