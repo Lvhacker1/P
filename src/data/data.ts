a8c5f0d63fc5bdca3734db77e6d80ea34f464071
Lvhacker1 <lavasha2@hotmail.com>
2025-08-28 11:45:12 +0200

    add social links data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,8 +1,13 @@
-import { NavLink } from "@/types/types";
+import { NavLink, SocialLinks } from "@/types/types";
 
 export const navLinks: NavLink[] = [
   { name: "Home", path: "/" },
   { name: "l", path: "/l" },
   { name: "ll", path: "/ll" },
   { name: "lll", path: "/lll" },
+];
+
+export const socialLinks: SocialLinks[] = [
+    {name: "GitHub", url: "", icon: "github"},
+    {name: "LinkedIn", url: "", icon: "linkedin"}
 ];
