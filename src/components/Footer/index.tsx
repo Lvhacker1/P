f3a29df7f1bd23faf040840fe0c8f7b4d7b30dc2
Lvhacker1 <lavasha2@hotmail.com>
2025-08-28 13:27:58 +0200

    add som styling to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,24 +6,23 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer>
-            <nav>
+        <footer className="bg-blue-600 text-white p-4 flex flex-col justify-between items-center gap-4">
+            <nav className="flex gap-4">
                 {navLinks.map((link: NavLink) => (
                     <Link key={link.path} href={link.name}>
                         {link.name}
                     </Link>
                 ))}
             </nav>
-            <div>
+            <div className="flex gap-4 cursor-grab">
                 {socialLinks.map((socialLink: SocialLinks) => (
                     <a key={socialLink.name} href={socialLink.url} target="_blank" rel="noopener noreferrer">
-                        {socialLink.name}
                         <Image src={socialLink.icon} alt={socialLink.name} width={24} height={24} />
                     </a>
                 ))}
             </div>
             <div>
-                <p>© {copyright.year} | {copyright.name} | {copyright.text}</p>
+                <p className="text-sm">© {copyright.year} | {copyright.name} | {copyright.text}</p>
             </div>
         </footer>
     )
